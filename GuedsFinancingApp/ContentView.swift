3a64f81de23550c4f0abf5d4853177aee0570c39
joaovictorguedes <148361905+joaovictorguedes@users.noreply.github.com>
2025-05-24 14:59:27 -0300

    initial commit

---
+++ GuedsFinancingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuedsFinancingApp
+//
+//  Created by João Victor de Souza Guedes on 24/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuedsFinancingApp/GuedsFinancingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuedsFinancingAppApp.swift
+//  GuedsFinancingApp
+//
+//  Created by João Victor de Souza Guedes on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GuedsFinancingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
